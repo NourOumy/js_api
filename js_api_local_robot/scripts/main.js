77aa3a277af6247e112767c1af902d4658e45279
NourOumy <ryahi.nourelhouda93@gmail.com>
2024-02-15 13:04:42 +0100

    fin js

--- js_api_local_robot/scripts/main.js
+++ js_api_local_robot/scripts/main.js
@@ -1,9 +1,9 @@
-// intégrer des plugin
-let wrapper = document.querySelector('.wrapper')
-let femalebtn = document.querySelector('.frobots')
-let malebtn = document.querySelector('.hrobots')
-let everybtn = document.querySelector('.everyrobots')
+// lien correction prof : https://www.youtube.com/watch?v=sHN3VABm6IM&ab_channel=OlivierCéréssia
 
+
+let wrapper = document.querySelector('.wrapper')
+let boutons = document.querySelector('.buttons')
+let result = document.querySelector('.result')
 
 function myFetch (gender){
 
@@ -11,18 +11,23 @@ fetch("scripts/robots.json")
 
   .then(response => response.json()) 
   .then(data => { 
+
     data.robots.forEach(function(robot){
+      let robotGender = robot.gender
+      if(gender == null|| gender == "all" || gender == robotGender){
       wrapper.innerHTML += `
       <div class="single-robot ${robot.gender}" data-gender="${robot.gender}">
       <h2>${robot.first_name} ${robot.last_name}</h2>
       <img src="${robot.portrait}" alt="robot">
       <p class ="description">${robot.description}</p>
         </div>
-      ` 
-      wrapper.
+     
+        ` 
+    }
 
-      if()
-
+    let nbrFemale = document.querySelectorAll('.Female')
+    let nbrMale = document.querySelectorAll('.Male')
+    result.innerHTML = `Il y a ${nbrFemale.length} femmes et ${nbrMale.length} hommes.`
     })
     
   })
@@ -32,3 +37,16 @@ fetch("scripts/robots.json")
 }
 
 myFetch()
+
+boutons.addEventListener("click", function(e){
+  if(e.target.hasAttribute('data-gender')){
+    wrapper.innerHTML =''
+    if(e.target.getAttribute == "all"){
+      myFetch()
+      
+    }else{
+      myFetch(e.target.getAttribute('data-gender'))
+    }
+    
+  }
+})
